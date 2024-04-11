b548bbf623da7bbc8492d98a8c55b89ee9605574
Hung Nguyen <nguyenhungbntb@gmail.com>
2024-04-11 22:57:40 +0700

    fix bug

--- app/src/main/java/com/vnc/flashscorex/ui/launch/LaunchActivity.kt
+++ app/src/main/java/com/vnc/flashscorex/ui/launch/LaunchActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vnc.flashscorex.R
 import com.vnc.flashscorex.databinding.ActivityLaunchBinding
 import com.vnc.flashscorex.ui.dashboard.DashBoardActivity
+import com.vnc.flashscorex.utils.GetCurrent
 
 
 class LaunchActivity : AppCompatActivity() {
@@ -18,20 +19,27 @@ class LaunchActivity : AppCompatActivity() {
     private lateinit var binding:ActivityLaunchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        installSplashScreen()
         binding = ActivityLaunchBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.hide()
-        window.statusBarColor = ContextCompat.getColor(this, R.color.status_splash)
-//        startActivity(Intent(this,DashBoardActivity::class.java))
+        window.statusBarColor = ContextCompat.getColor(this, R.color.back_ground_main)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val options = ActivityOptions.makeSceneTransitionAnimation(this)
             val intent = Intent(this,DashBoardActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent,options.toBundle())
             startActivity(intent)
-        }, 4000)
+        }, 2000)
+
+        setUpView()
 
     }
+
+    private fun setUpView(){
+        binding.txtDate.text = GetCurrent.getDate()
+        binding.txtTime.text = GetCurrent.getTimeHours()
+    }
+
 }
